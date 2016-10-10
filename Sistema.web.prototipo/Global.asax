49eca937886072eb3d7afde8c10c1d852f384b20
filipe.rossell <filipe.rossell@MT01LRTI01>
2016-10-10 17:44:47 -0400

    Adicionar arquivos de projeto.

---
+++ Sistema.web.prototipo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sistema.web.prototipo.MvcApplication" Language="C#" %>
---
+++ Sistema.web.prototipo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema.web.prototipo.Startup))]
+namespace Sistema.web.prototipo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
